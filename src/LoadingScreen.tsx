5c2dd12bff7b8a0920e1c13a756e59c6e42acaf3
Gamezordd <amartyamishra8@gmail.com>
2026-02-05 21:20:28 +0530

    padding for initial loader 2;

--- src/LoadingScreen.tsx
+++ src/LoadingScreen.tsx
@@ -1,9 +1,9 @@
 const LoadingScreen = ({ socketLoading }: { socketLoading: boolean }) => {
   return (
-    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900/50 backdrop-blur-lg z-50 px-2">
+    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900/50 backdrop-blur-lg z-50 px-3">
       <div className="bg-gray-800 p-8 rounded-lg shadow-xl flex flex-col items-center space-y-4 border border-gray-700">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-        <h1 className="text-3xl font-bold text-white">
+        <h1 className="text-3xl font-bold text-white text-center">
           Connecting to server...
         </h1>
         {socketLoading ? <p className="text-gray-400 text-center">Establishing socket connection...</p> : (
