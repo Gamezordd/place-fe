19c0d04146e7cf0c503e9f8d5d46bcef07350cf8
Gamezordd <amartyamishra8@gmail.com>
2026-02-05 21:15:29 +0530

    padding for initial loader

--- src/LoadingScreen.tsx
+++ src/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 const LoadingScreen = ({ socketLoading }: { socketLoading: boolean }) => {
   return (
-    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900/50 backdrop-blur-lg z-50">
+    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900/50 backdrop-blur-lg z-50 px-2">
       <div className="bg-gray-800 p-8 rounded-lg shadow-xl flex flex-col items-center space-y-4 border border-gray-700">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
         <h1 className="text-3xl font-bold text-white">
@@ -9,7 +9,7 @@ const LoadingScreen = ({ socketLoading }: { socketLoading: boolean }) => {
         {socketLoading ? <p className="text-gray-400 text-center">Establishing socket connection...</p> : (
           <>
             <p className="text-gray-400 text-center">
-              Please wait while we establish a connection.
+              Please wait while a connection is established.
             </p>
             <p className="text-gray-400 text-center">
               This service runs on a free-tier server and sleeps after
